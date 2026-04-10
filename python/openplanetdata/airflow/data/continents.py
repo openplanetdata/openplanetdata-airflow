37a2b9620b86bb2b34374424cae7a8a206df79e8
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2026-04-11 00:34:10 +0200

    Add continent code field

--- python/openplanetdata/airflow/data/continents.py
+++ python/openplanetdata/airflow/data/continents.py
@@ -1,9 +1,9 @@
 CONTINENTS = [
-    {"name": "Africa", "slug": "africa"},
-    {"name": "Antarctica", "slug": "antarctica"},
-    {"name": "Asia", "slug": "asia"},
-    {"name": "Europe", "slug": "europe"},
-    {"name": "North America", "slug": "north-america"},
-    {"name": "Oceania", "slug": "oceania"},
-    {"name": "South America", "slug": "south-america"},
+    {"code": "AF", "name": "Africa", "slug": "africa"},
+    {"code": "AN", "name": "Antarctica", "slug": "antarctica"},
+    {"code": "AS", "name": "Asia", "slug": "asia"},
+    {"code": "EU", "name": "Europe", "slug": "europe"},
+    {"code": "NA", "name": "North America", "slug": "north-america"},
+    {"code": "OC", "name": "Oceania", "slug": "oceania"},
+    {"code": "SA", "name": "South America", "slug": "south-america"},
 ]
